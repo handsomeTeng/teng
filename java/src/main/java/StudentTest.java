cacdfe366c2356e5097888269c583310c5df61f4
zhangtao <taozhang1029@qq.com>
2021-07-13 15:57:17 +0800

    添加getter setter toString 方法，添加测试类

---
+++ java/src/main/java/StudentTest.java
@@ -0,0 +1,17 @@
+/**
+ * Created by zhangtao552 on 2021/7/13.
+ */
+public class StudentTest {
+
+    public static void main(String[] args) {
+
+        Student student = new Student();
+
+        student.setAge(28);
+        student.setUserName("zhangsan");
+
+        System.out.println(student);
+
+    }
+
+}
